8492aea04b71851f5688f81bb46d01b9ca2ba346
walessy <amoswales@gmail.com>
2025-08-31 11:13:08 +0100

    Add missing Jest setup file for integration tests

---
+++ tests/setup.js
@@ -0,0 +1,22 @@
+﻿// tests/setup.js
+// Jest setup file for integration tests
+
+// Global test timeout
+jest.setTimeout(30000);
+
+// Setup before all tests
+beforeAll(() => {
+  console.log('Setting up integration tests...');
+});
+
+// Cleanup after all tests  
+afterAll(() => {
+  console.log('Cleaning up integration tests...');
+});
+
+// Mock console methods if needed
+// global.console = {
+//   log: jest.fn(),
+//   error: jest.fn(),
+//   warn: jest.fn(),
+// };
