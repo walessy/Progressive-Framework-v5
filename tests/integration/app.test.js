c5063ec6e81d6475d8329559af73bc6f1b5d9c3f
walessy <amoswales@gmail.com>
2025-08-31 11:26:13 +0100

    Add basic integration tests and --passWithNoTests flag

---
+++ tests/integration/app.test.js
@@ -0,0 +1,15 @@
+﻿// tests/integration/app.test.js
+describe('Integration Tests', () => {
+  it('should pass basic integration test', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should validate environment setup', () => {
+    expect(process.env.NODE_ENV).toBeDefined();
+  });
+
+  // Add more integration tests as your app grows
+  it('should have required dependencies', () => {
+    expect(() => require('../../package.json')).not.toThrow();
+  });
+});
