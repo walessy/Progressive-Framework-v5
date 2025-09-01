f63f12204994fffc40ad37f8e1dc65ca8a557803
walessy <amoswales@gmail.com>
2025-09-01 14:18:17 +0100

    c

--- src/agents/MasterControlAgent.js
+++ src/agents/MasterControlAgent.js
@@ -19,7 +19,7 @@ class MasterControlAgent {
         this.systemHealth = 'healthy';
         
         // Initialize synchronously for immediate availability
-        this.initializeAgents();npm testcls
+        this.initializeAgents();
         this.emergencyInterface = this.emergencySystem.getEmergencyInterface();
         this.setupEmergencyHandlers();
     }
